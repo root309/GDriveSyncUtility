3f737c84e812344af54d00ec8415aafc1db593c4
root309 <choco00101x@gmail.com>
2023-09-05 07:08:35 +0900

    Update: サブフォルダなどの一括ダウンロードに対応

--- download.py
+++ download.py
@@ -4,27 +4,35 @@ import io
 from googleapiclient.http import MediaIoBaseDownload
 import os
 
-def download_files_from_folder(service, folder_id, local_folder_path):
-    if not os.path.exists(local_folder_path):
-        os.makedirs(local_folder_path)
+def download_files_from_folder(service, folder_id, local_folder_path): # Driveの特定のフォルダをダウンロードする関数
+    if not os.path.exists(local_folder_path): # ローカルのダウンロード先フォルダの存在確認条件文
+        os.makedirs(local_folder_path) # 存在しない場合は作成
 
-    # フォルダ内のファイルを取得
-    results = service.files().list(q=f"'{folder_id}' in parents", fields="files(id, name)").execute()
+    # フォルダ内のファイルとサブフォルダを取得
+    results = service.files().list(q=f"'{folder_id}' in parents", fields="files(id, name, mimeType)").execute()
     items = results.get('files', [])
 
     for item in items:
-        file_id = item['id']
-        request = service.files().get_media(fileId=file_id)
+        file_id = item['id'] # ファイルまたはフォルダの一意な識別子
+        file_name = item['name'] # ファイルまたはフォルダの名前
+        mime_type = item['mimeType'] # ファイルのMIMEタイプ
+        # ファイルorフォルダの識別
+        if mime_type == 'application/vnd.google-apps.folder':
+            # サブフォルダの場合、再帰的にダウンロード
+            new_local_folder_path = os.path.join(local_folder_path, file_name)
+            download_files_from_folder(service, file_id, new_local_folder_path)
+        else:
+            # ファイルの場合、ダウンロード
+            request = service.files().get_media(fileId=file_id)
+            fh = io.FileIO(os.path.join(local_folder_path, file_name), 'wb')
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                print(f"ダウンロード完了 {file_name}: {int(status.progress() * 100)}%.")
 
-        fh = io.FileIO(f"{local_folder_path}/{item['name']}", 'wb')
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while not done:
-            status, done = downloader.next_chunk()
-            print(f"ダウンロード完了 {item['name']}: {int(status.progress() * 100)}%.")
-
-# サービスアカウントの認証
-credentials = Credentials.from_service_account_file("jsonのapikeyのローカルパスを入力",
+# API認証
+credentials = Credentials.from_service_account_file("C:\\path\\to", # ダウンロードしたAPI認証キーのjsonファイルのPath
     scopes=["https://www.googleapis.com/auth/drive.readonly"])
 
 # Google Drive API クライアントを構築
